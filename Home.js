c40ac5849b3bb9ff7fc450fff84338beb3ff0758
Seeshirt <123482068+Seeshirt@users.noreply.github.com>
2024-05-02 18:59:50 +0530

    Added

--- Home.js
+++ Home.js
@@ -10,3 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+
+let slideIndex = 0;
+
+function changeSlide(n) {
+    slideIndex += n;
+    showSlides();
+}
+
+function showSlides() {
+    let slides = document.querySelectorAll('.slide');
+    let totalSlides = slides.length;
+
+    if (slideIndex >= totalSlides) {
+        slideIndex = 0;
+    }
+    if (slideIndex < 0) {
+        slideIndex = totalSlides - 0;
+    }
+
+    let offset = -slideIndex * 13 + '%';
+    document.querySelector('.Slider-content').style.transform = 'translateX(' + offset + ')';
+}
+
+showSlides(); 
